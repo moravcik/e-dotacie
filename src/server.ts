25656fb9c67eefb20d1951c166ef449d1699cabf
Michal Moravcik <miso.moravcik@gmail.com>
2020-02-25 09:23:22 +0100

    added meta viewport

--- src/server.ts
+++ src/server.ts
@@ -25,6 +25,7 @@ export default function startServer() {
     res.send(`
 <html>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     ${gaSnippet}
   </head>
   <body style="margin: 0px; padding: 0px">
@@ -39,6 +40,7 @@ export default function startServer() {
     res.send(`
 <html>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     ${gaSnippet}
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   </head>
