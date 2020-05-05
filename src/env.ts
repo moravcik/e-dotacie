4350f568f897aaa6d2f5ee7b5ec843d440addfd2
Michal <miso.moravcik@gmail.com>
2020-05-06 00:34:26 +0200

    updated XPATH

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 
 const DEFAULT_PAGE_URL = 'https://www.mhsr.sk/ministerstvo/dotacie/dotacie-v-roku-2019';
-const DEFAULT_XPATH_PDF_LABEL = 'string(/html/body/section[2]/div/article/div/ul/li[20]/ul/li[24]/a/div/text())';
-const DEFAULT_XPATH_PDF_URL = 'string(/html/body/section[2]/div/article/div/ul/li[20]/ul/li[24]/a/@href)';
+const DEFAULT_XPATH_PDF_LABEL = 'string(/html/body/section[2]/div/article/div/ul/li[20]/ul/li[22]/a/div/text())';
+const DEFAULT_XPATH_PDF_URL = 'string(/html/body/section[2]/div/article/div/ul/li[20]/ul/li[22]/a/@href)';
 const DEFAULT_EXPIRATION_MILLIS = '600000'; // 10 mins
 
 export const environment = process.env.NODE_ENV || 'development';
